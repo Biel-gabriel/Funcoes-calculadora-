14b342b4e7002e0c1c0bca103df11a04104cbca2
Biel-gabriel <gabrielelianas2@gmail.com>
2025-04-15 21:25:45 -0300

    Add files via upload

---
+++ script.js/funcoes/script.js
@@ -0,0 +1,7 @@
+function soma(num1, num2) {
+    return num1 + num2
+
+}
+
+let resultado = soma(10,20)
+alert(num1 + nun2)
---
+++ script.js/var-let-const/script.js
@@ -0,0 +1,12 @@
+function teste() {
+if(true) {
+    var a = 10 
+    let b = 20
+    console.log(resultado)
+}
+let resultado = 30
+console.log(a)
+console.log(b)
+
+}
+teste()
